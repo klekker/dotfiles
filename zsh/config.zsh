77a4e2b9578d95c69243edf2c18848be53c1c38b
klekker <klekker@gmail.com>
2015-02-19 10:04:38 +0100

    fixed zsh stuff

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -2,7 +2,7 @@ unsetopt beep
 bindkey -v
 bindkey "^R" history-incremental-search-backward
 
-source /usr/local/bin/aws_zsh_completer.sh
+# source /usr/local/bin/aws_zsh_completer.sh
 
 setopt HIST_IGNORE_DUPS
 setopt no_prompt_cr
@@ -10,12 +10,5 @@ HISTFILE=$HOME/.zsh_histfile
 HISTSIZE=2000
 SAVEHIST=50000
 
-if [[ $TERM = 'screen' ]]
-then
-	PROMPT="%B(${WINDOW}) [%T]  %n@%m:%~
-$%b " 
-
-else 
-	PROMPT='%B[%T] %n@%m:%~
-$%b ' 
-fi
+PROMPT='%B[%T] %n@%m:%~
+$%b '
